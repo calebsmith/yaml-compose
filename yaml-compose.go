dbf424673d8009460ed630cab90a1ba64c5bb49b
Caleb Smith <caleb.smithnc@gmail.com>
2023-07-08 16:50:06 -0400

    WIP - Adds linewise pre-processing step

--- yaml-compose.go
+++ yaml-compose.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"log"
+	//"regexp"
+	"bufio"
+	"os"
+	"strings"
 )
 
+func pre_process(raw_file string) (string, error) {
+	// TBI: No-op for now
+	return raw_file, nil
+}
+
+/*
 func process(out interface{}) {
 	switch i := out.(type) {
 	case nil, bool, int, float32, float64:
@@ -29,22 +38,47 @@ func process(out interface{}) {
 		log.Fatalf("Unhandled type %T for value %+v", i, i)
 	}
 }
+*/
 
 func main() {
 
-	// Load and parse file
-	content, err := ioutil.ReadFile("example.yaml")
+	// Load file
+	file, err := os.Open("example.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
+
+	// Pre-process linewise
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		raw_line := scanner.Text()
+		processed_line, err := pre_process(raw_line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lines = append(lines, processed_line)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	processed_out := strings.Join(lines, "\n")
+
+	log.Printf("Pre-processed:\n")
+	log.Printf(processed_out)
+
+	// Parse as yaml
 	yaml_in := yaml.MapSlice{}
-	if err := yaml.Unmarshal([]byte(content), &yaml_in); err != nil {
+	if err := yaml.Unmarshal([]byte(processed_out), &yaml_in); err != nil {
 		log.Fatal(err)
 	}
 
+	// FIXME: Consider removal
 	// Process file
-	process(yaml_in)
+	// process(yaml_in)
 
+	// FIXME: Consider just using processed_out directly after verification
 	// Output results
 	out, err := yaml.Marshal(yaml_in)
 	if err != nil {
